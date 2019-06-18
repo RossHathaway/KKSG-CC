d710b8c9eb5b82638d5bc2b60f8d756e0d65abad
Ross Hathaway <hathaway.ross@gmail.com>
2019-06-18 01:06:39 -0400

    display image in Our Practice page.
    Images are more blurry on website than on desktop - way too blurry. There must be a problem with application, not images.

--- src/pages/practice.js
+++ src/pages/practice.js
@@ -1,14 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
 import { css } from "@emotion/core"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const PracticePage = () => (
+const PracticePage = ({ data }) => (
   <Layout>
     <SEO title="About our practice" />
     <h1>Our Sunday Practice</h1>
+    <Img
+      fluid={data.file.childImageSharp.fluid}
+      alt="image of Chenrezig, the Buddha of Compassion"
+      fadeIn="soft"
+    />
     <p>Everyone is welcome to join us each Sunday from 11 am - 12 pm.</p>
     <p>
       Our meditation involves doing a sadhana, which is a type of meditation
@@ -34,3 +40,15 @@ const PracticePage = () => (
 )
 
 export default PracticePage
+
+export const pageQuery = graphql`
+  query {
+    file(relativePath: { eq: "BuddhaStatue-L U C H I C U O N G.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 100) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
