29cb671990e1587f0202011461e4052bf451e2db
Ross Hathaway <hathaway.ross@gmail.com>
2019-07-15 11:58:15 -0400

    show Chenrezig picture on "Practice" page with text next to it if space, and text below if not enough space

--- src/pages/practice.js
+++ src/pages/practice.js
@@ -10,39 +10,74 @@ const PracticePage = props => (
   <Layout>
     <SEO title="About our practice" />
     <h1>Our Sunday Practice</h1>
-    <div />
-    <Img
-      fluid={props.data.Chenrezig.childImageSharp.fluid}
-      alt="image of Chenrezig, the Buddha of Compassion"
-      fadeIn={true}
-      style={{
-        // ...(props.style || {}),
-        maxWidth: "400px",
-        /*props.data.Chenrezig.childImageSharp.fluid
-            .presentationWidth,*/
-        // margin: "0 auto", // Used to center the image
-      }}
-    />
-    <p>Everyone is welcome to join us each Sunday from 11 am - 12 pm.</p>
-    <p>
-      Our meditation involves doing a sadhana, which is a type of meditation
-      that involves visualization, prayer, mantras, and silent meditation. The
-      sadhana we practice is common in the Kagyu school of Tibetan Buddhism; it
-      is a traditional sadhana of Chenrezig, the Buddha of compassion.
-    </p>
     <div
       css={css`
-        p {
-          margin: 0.2rem;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: flex-start;
+        & > * {
+          /* flex-grow: 1; */
+          /* flex-shrink: 1; */
+          flex: 1 1 auto;
+          min-width: 200px;
         }
       `}
     >
-      <p>
-        Location: <a href="https://evokeyogacapecod.com/">Evoke Yoga</a>
-      </p>
-      <p>64 Enterprise Road</p>
-      <p>Hyannis, MA 02601</p>
-      <p>U.S.A.</p>
+      <Img
+        fluid={props.data.Chenrezig.childImageSharp.fluid}
+        alt="image of Chenrezig, the Buddha of Compassion"
+        fadeIn={true}
+        style={{
+          // ...(props.style || {}),
+          maxWidth: "400px",
+          // overflow: "visible !important",
+          // display: "inlineBlock",
+          // ...props.data.Chenrezig.childImageSharp.fluid.presentationWidth,
+          // margin: "0 auto", // Used to center the image
+        }}
+      />
+      <div
+        css={css`
+          /* width: 300px; */
+          width: 350px;
+        `}
+      >
+        <p>Everyone is welcome to join us each Sunday from 11 am - 12 pm.</p>
+        <p>
+          Our meditation involves doing a sadhana, which is a type of meditation
+          that involves visualization, prayer, mantras, and silent meditation.
+          The sadhana we practice is common in the Kagyu school of Tibetan
+          Buddhism; it is a traditional sadhana of Chenrezig, the Buddha of
+          compassion.
+        </p>
+        <section
+          css={css`
+            p {
+              margin: 0.2rem;
+            }
+          `}
+        >
+          <p>
+            Location: <a href="https://evokeyogacapecod.com/">Evoke Yoga</a>
+          </p>
+          <p>64 Enterprise Road</p>
+          <p>Hyannis, MA 02601</p>
+          <p>U.S.A.</p>
+        </section>
+      </div>
+      {/* <Img
+        fluid={props.data.Chenrezig.childImageSharp.fluid}
+        alt="image of Chenrezig, the Buddha of Compassion"
+        fadeIn={true}
+        style={{
+          // ...(props.style || {}),
+          maxWidth: "400px",
+          overflow: "visible !important",
+          display: "inlineBlock",
+          // ...props.data.Chenrezig.childImageSharp.fluid.presentationWidth,
+          margin: "0 auto", // Used to center the image
+        }}
+      /> */}
     </div>
     <Img
       fluid={props.data.groupPic.childImageSharp.fluid}
@@ -75,7 +110,7 @@ export const pageQuery = graphql`
     }
     Chenrezig: file(relativePath: { eq: "Chenrezig.jpg" }) {
       childImageSharp {
-        fluid(maxWidth: 500) {
+        fluid(maxWidth: 400) {
           ...GatsbyImageSharpFluid_withWebp
           presentationWidth
         }
